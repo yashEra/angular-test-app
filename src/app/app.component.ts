5246b610098226d953e96a60b01f045e30db267a
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2025-03-31 22:10:05 +0530

    Navbar Create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'})
+  styleUrl: './app.component.css',
+})
 export class AppComponent {
   title = 'my-angular-app';
-  src ="assets/images/logo.png";
-  alt ="Logo";
 }
